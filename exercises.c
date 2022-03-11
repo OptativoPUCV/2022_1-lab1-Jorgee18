ca48beaf6c371d8b5b26c7077a5ba0de35758e68
Your Name <you@example.com>
2022-03-11 15:21:51 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -97,7 +97,12 @@ la cual asigna el valor a la posición i del vector v.
 */
 void asignarValor(Vector * v, int i, int valor) 
 {
-   
+   v = NULL;
+   v = (Vector*)malloc(i * sizeof(Vector));
+   v[i]->datos = NULL;
+   v[i]->datos = (int*) calloc(sizeof(int));
+   v[i]->datos[i] = valor;
+
 }
 
 /*
