9192d943e01202edc892c48a7aa5bcb597335fa5
Your Name <you@example.com>
2022-03-11 15:29:20 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -99,10 +99,7 @@ void asignarValor(Vector * v, int i, int valor)
 {
    v = NULL;
    v = (Vector*)malloc(i * sizeof(Vector));
-   v[i].datos = NULL;
-   v[i].datos = (int*) calloc(i, sizeof(int));
-   v[i].capacidad = valor;
-
+   v[i].capacidad = valor; 
 }
 
 /*
